ebb18c68590303efb3d16e81f9270aba122b7b72
Zhiming Jiang <lanvige@gmail.com>
2020-04-28 16:42:42 +0800

    ✨ add base64

--- base64_util.go
+++ base64_util.go
@@ -1,1 +1,15 @@
 package utils
+
+import (
+	"encoding/base64"
+)
+
+// Base64Decode Base64Decode
+func Base64Decode(value string) (string, error) {
+	decoded, err := base64.StdEncoding.DecodeString(value)
+	if nil != err {
+		return "", err
+	}
+
+	return string(decoded), nil
+}
